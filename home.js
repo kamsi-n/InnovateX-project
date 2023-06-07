894190e415d7b2987fbcddfc779e4fb044760a0c
kamsi-n01~ <nwokorokamsi21@gmail.com>
2023-06-07 17:21:13 +0100

    create 1st section

---
+++ home.js
@@ -0,0 +1,6 @@
+document.getElementById('toggleBtn').addEventListener('click', () => {
+    const navbarItems = document.getElementById('navbarItems');
+    navbarItems.classList.toggle('show');
+    navbarItems.classList.toggle('hide');
+  });
+  
