0d3059f17615420bfd7be12ed5bb023cae78a439
PeculiarW <peculiarw9@gmail.com>
2023-06-15 11:40:34 +0100

    update

---
+++ footerscript.js
@@ -0,0 +1,9 @@
+function submitEmail() {
+    //Retrieve the value entered in the email input field
+    var email =
+    document.getElementById("p-box").value;
+
+    //Perform validation or further processing with the email value
+    //...
+    //Clear the input field
+}
