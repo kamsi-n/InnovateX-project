afe4cc4af9254366d2ae4ca6ba3bab29d8dc7521
kamsi-n01~ <nwokorokamsi21@gmail.com>
2023-06-15 19:58:09 +0100

    add footer

---
+++ resources/script/dashboard.js
@@ -0,0 +1,12 @@
+const links = document.querySelectorAll('.db-sidebar a');
+links.forEach(link => {
+  const icon = link.querySelector('img');
+  const originalSrc = icon.src;
+  const hoverSrc = icon.dataset.srcHover;
+  link.addEventListener('mouseover', () => {
+    icon.src = hoverSrc;
+  });
+  link.addEventListener('mouseout', () => {
+    icon.src = originalSrc;
+  });
+});
