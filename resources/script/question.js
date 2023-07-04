855af6c914d2fe6c7b4ddf3fde3309ca1ea9e036
kamsi-n01~ <nwokorokamsi21@gmail.com>
2023-07-04 10:49:09 +0100

    add the question page

--- resources/script/question.js
+++ resources/script/question.js
@@ -3,3 +3,20 @@ button.addEventListener('click', () => {
   const nav = document.getElementById('rightSide');
   nav.style.visibility = nav.style.visibility === 'visible' ? 'hidden' : 'visible';
 });
+
+let timeLeft = 15;
+
+const timerInterval = setInterval(updateTimer, 1000);
+
+function updateTimer() {
+  const timerElement = document.getElementById('timer');
+
+  timerElement.innerHTML = `${timeLeft}:00`;
+
+  if (timeLeft === 0) {
+    clearInterval(timerInterval);
+    timerElement.innerHTML = '00:00';
+  } else {
+    timeLeft--;
+  }
+}
