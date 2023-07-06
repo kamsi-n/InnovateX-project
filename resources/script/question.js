38e08400592d5d136e7de294a27d249cf27441a3
kamsi-n01~ <nwokorokamsi21@gmail.com>
2023-07-06 13:49:31 +0100

    complete question.html

--- resources/script/question.js
+++ resources/script/question.js
@@ -6,8 +6,6 @@ button.addEventListener('click', () => {
 
 let timeLeft = 15;
 
-const timerInterval = setInterval(updateTimer, 1000);
-
 function updateTimer() {
   const timerElement = document.getElementById('timer');
 
@@ -17,6 +15,51 @@ function updateTimer() {
     clearInterval(timerInterval);
     timerElement.innerHTML = '00:00';
   } else {
-    timeLeft--;
+    timeLeft -= 1;
   }
 }
+
+const timerInterval = setInterval(updateTimer, 1000);
+
+const publish = document.getElementById('publish');
+const popup1 = document.getElementById('loading');
+const overlay = document.getElementById('getCodeLink');
+
+publish.addEventListener('click', () => {
+  popup1.style.visibility = 'visible';
+  overlay.style.visibility = 'hidden';
+  setTimeout(() => {
+    popup1.style.visibility = 'hidden';
+    overlay.style.visibility = 'visible';
+  }, 3000);
+});
+
+const close = document.getElementById('close');
+const send = document.getElementById('send');
+const sendOverlay = document.getElementById('sendOverlay');
+close.addEventListener('click', () => {
+  overlay.style.visibility = 'hidden';
+});
+
+send.addEventListener('click', () => {
+  sendOverlay.style.visibility = 'visible';
+});
+
+const close2 = document.getElementById('close2');
+const sendBtn = document.getElementById('sendBtn');
+
+close2.addEventListener('click', () => {
+  sendOverlay.style.visibility = 'hidden';
+});
+
+const alert = document.getElementById('sendAlert');
+sendBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  alert.style.visibility = 'visible';
+
+  setTimeout(() => {
+    alert.style.visibility = 'hidden';
+    window.location.href = '../html/dashboard.html';
+  }, 2000);
+});
